909fa39696255995c7bbca0715125725ccac3862
C@Z <cz.theng@gmail.com>
2014-08-28 10:13:08 +0800

    add utils directory and common define

---
+++ ccpp/utils/common_define.h
@@ -0,0 +1,11 @@
+#ifndef CZBASE_UTILS_COMMON_DEFINE_H_
+#define CZBASE_UTILS_COMMON_DEFINE_H_
+
+// OS Platform
+#define _OS_LINUX_   "linux"
+#define _OS_ANDROID_ "android"
+#define _OS_WIN32_   "win32"
+
+
+
+#endif
